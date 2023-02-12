04856f98b22813d185bff954f6e756092ca1451f
Ajit Sahoo <87249806+Ajit99999@users.noreply.github.com>
2023-02-12 11:12:17 -0800

    Update firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app'
 import {getAuth } from 'firebase/auth'
 import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
-    apiKey: "AIzaSyAmHR6BjBZ-grcwtqWsFEWeoqMiWAl7LnY",
-    authDomain: "ecommerceapp-163ee.firebaseapp.com",
-    projectId: "ecommerceapp-163ee",
-    storageBucket: "ecommerceapp-163ee.appspot.com",
-    messagingSenderId: "923624056687",
-    appId: "1:923624056687:web:15f4de5e835dff136a6a9a"
+    apiKey: "****************************",
+    authDomain: "****************************",
+    projectId: "****************************",
+    storageBucket: "****************************",
+    messagingSenderId: "****************************",
+    appId: "****************************"
   };
 
 export const app = initializeApp(firebaseConfig);
